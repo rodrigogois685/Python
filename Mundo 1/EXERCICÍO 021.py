529cdf6d2e59dcd20d40a8839770cf2db5e1c1e7
rodrigogois685 <71515548+rodrigogois685@users.noreply.github.com>
2022-03-17 10:23:53 -0300

    Update EXERCICÍO 021.py

--- Mundo 1/EXERCICÍO 021.py
+++ Mundo 1/EXERCICÍO 021.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-pygame.mixer.music.load('C:\Users\rodrigo.gois\OneDrive\Área de Trabalho\estudos\Curso--Python\Exercicíos\Mundo 1/musica_do_sherek.mp3')
+pygame.mixer.music.load('musica_do_sherek.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
 print('Tocando agora a música Smash Mouth do All Star ')
