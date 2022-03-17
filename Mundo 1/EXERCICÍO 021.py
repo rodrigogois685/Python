b38ae0ba1a35dba0dbb19d027bc930a823657915
rodrigogois685 <71515548+rodrigogois685@users.noreply.github.com>
2022-03-17 10:11:30 -0300

    Update EXERCICÍO 021.py

--- Mundo 1/EXERCICÍO 021.py
+++ Mundo 1/EXERCICÍO 021.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-pygame.mixer.music.load('C:/Users/rodri/OneDrive/Área de Trabalho/banhodoscampeoes.mpeg')
+pygame.mixer.music.load('C:\Users\rodrigo.gois\OneDrive\Área de Trabalho\estudos\Curso--Python\Exercicíos\Mundo 1/musica_do_sherek.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
 print('Tocando agora o áudio do Banhão dos Campeões')
